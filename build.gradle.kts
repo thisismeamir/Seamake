d3473e091336ea41656ffaab1cc3d975149f0c8f
Kid A <thisismeamir@outlook.com>
2025-12-30 09:15:45 +0330

    [fix] release

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.github.thisismeamir.seemake"
-version = System.getenv("VERSION") ?: "1.0.0"
+version = System.getenv("VERSION") ?: "1.0.2"
 
 allprojects {
     group = rootProject.group
