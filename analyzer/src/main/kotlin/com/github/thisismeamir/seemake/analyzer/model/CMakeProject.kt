5eddbf414ea03063bedf77abf228ef7dbb37fb88
Kid A <thisismeamir@outlook.com>
2026-01-05 17:19:50 +0330

    [implementation] language management and further analysis features

--- analyzer/src/main/kotlin/com/github/thisismeamir/seemake/analyzer/model/CMakeProject.kt
+++ analyzer/src/main/kotlin/com/github/thisismeamir/seemake/analyzer/model/CMakeProject.kt
@@ -1,4 +1,7 @@
 package com.github.thisismeamir.seemake.analyzer.model
+
+import java.io.File
+
 /**
  * Represents a complete CMake project with all its components
  */
@@ -10,12 +13,33 @@ data class CMakeProject(
     val projectVersion: String?,
     val projectDescription: String?,
     val languages: List<String>,
-    val targets: List<Target>,
+    val cmakeTargets: List<CMakeTarget>,
     val dependencies: List<Dependency>,
     val options: List<Option>,
     val variables: Map<String, String>,
     val subprojects: List<CMakeProject>,
     val cmakeFiles: List<String>,
     val cmakeMinimumVersion: String?
-)
+) {
+    // The equality criteria of Two CMakeProjects
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is CMakeProject) return false
+
+        if (File(rootDirectory).absolutePath.toString() != File(other.rootDirectory).absolutePath) return false
+        if (projectName != other.projectName) return false
+        if (projectVersion != other.projectVersion) return false
+        if (projectDescription != other.projectDescription) return false
+        if (languages.sorted() != other.languages.sorted()) return false
+        if (cmakeTargets.sortedBy { it.name } != other.cmakeTargets.sortedBy { it.name }) return false
+        if (dependencies.sortedBy { it.name } != other.dependencies.sortedBy { it.name }) return false
+        if (options.sortedBy { it.name } != other.options.sortedBy { it.name }) return false
+        if (variables != other.variables) return false
+        if (subprojects.sortedBy { it.rootDirectory } != other.subprojects.sortedBy { it.rootDirectory }) return false
+        if (cmakeFiles.sorted() != other.cmakeFiles.sorted()) return false
+        if (cmakeMinimumVersion != other.cmakeMinimumVersion) return false
+
+        return true
+    }
+}
 
